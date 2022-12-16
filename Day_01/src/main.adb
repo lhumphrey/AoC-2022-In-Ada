295900168a8df605d1d1068bf0178f26ece67052
lhumphrey <humphrey.laura@gmail.com>
2022-12-16 10:52:38 -0500

    Day 01 in SPARK cleanup.

--- Day_01/src/main.adb
+++ Day_01/src/main.adb
@@ -1,7 +1,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO;
-with Ada.Numerics.Big_Numbers.Big_Integers;
-use Ada.Numerics.Big_Numbers.Big_Integers;
+
+pragma Warnings (Off, """*"" is set by ""*"" but not used after the call",
+                 Reason => "Unused parameter is mandated by the API");
 
 procedure Main with SPARK_Mode is
    Filename : String := "input.txt";
@@ -17,10 +18,10 @@ begin
    Open (File, In_File, Filename);
 
    while not End_Of_File (File) and not Error loop
-
       Get_Line (File, S, Last);
 
       if Last > 0 then
+         -- Continue summing items from the same elf.
          Ada.Integer_Text_IO.Get (S (1 .. Last), Element, Last);
          if (Element <= Integer'Last - Sum and then Element >= 0) then
             Sum := Sum + Element;
@@ -28,9 +29,11 @@ begin
             Error := True;
          end if;
       else
+         -- Check whether the sum is a maximum or top 3 maximum, then reset.
          if Sum > Maximum then
             Maximum := Sum;
          end if;
+
          if Sum >= Max1 then
             Max3 := Max2;
             Max2 := Max1;
@@ -41,13 +44,12 @@ begin
          elsif Sum >= Max3 then
             Max3 := Sum;
          end if;
+
          Sum := 0;
       end if;
-
    end loop;
 
-   if (To_Big_Integer (Max1) + To_Big_Integer (Max2) + To_Big_Integer (Max3) <
-         To_Big_Integer (Integer'Last))
+   if Max1 <= Integer'Last - Max2 and then Max3 <= Integer'Last - Max1 - Max2
    then
       Sum := Max1 + Max2 + Max3;
    else
@@ -57,8 +59,8 @@ begin
    if Error then
       Put_Line ("There was an error.");
    else
-      Put_Line ("Part 1, Maximum calories: " & Maximum'Image);
-      Put_Line ("Part 2, Sum of top three calories: " & Sum'Image);
+      Put_Line ("Maximum calories for Part 1: " & Maximum'Image);
+      Put_Line ("Sum of top three calories for Part 2: " & Sum'Image);
    end if;
 
    Close (File);
