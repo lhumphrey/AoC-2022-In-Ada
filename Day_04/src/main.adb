ba6b8410fef894789b4e3e4f95231d40415b9a09
lhumphrey <humphrey.laura@gmail.com>
2022-12-16 12:07:32 -0500

    Day 04 in SPARK cleanup.

--- Day_04/src/main.adb
+++ Day_04/src/main.adb
@@ -4,13 +4,16 @@ with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 
 procedure Main with SPARK_Mode is
 
+   pragma Warnings (Off, """*"" is set by ""*"" but not used after the call",
+                    Reason => "Unused parameter is mandated by the API");
+
    Filename : String := "input.txt";
    File : File_Type;
    Error : Boolean := False;
 
    S : String (1 .. 256);
    Comma, Hyphen1, Hyphen2, Line_Last, Num_Last : Natural;
-   Lower1, Upper1, Lower2, Upper2 : Integer;
+   X1, X2, Y1, Y2 : Integer;
 
    Num_Subset_Pairs : Natural := 0;
    Num_Overlapping_Pairs : Natural := 0;
@@ -32,16 +35,17 @@ begin
       end if;
 
       if Hyphen1 > 0 and then Hyphen2 > 0 and then Comma > 0 and then
-        Hyphen1 < Comma and then Hyphen2 > Comma
+         Hyphen1 < Comma and then Hyphen2 > Comma
       then
-         Ada.Integer_Text_IO.Get (S (1 .. Hyphen1 - 1), Lower1, Num_Last);
-         Ada.Integer_Text_IO.Get (S (Hyphen1 + 1 .. Comma - 1), Upper1, Num_Last);
-         Ada.Integer_Text_IO.Get (S (Comma + 1 .. Hyphen2 - 1), Lower2, Num_Last);
-         Ada.Integer_Text_IO.Get (S (Hyphen2 + 1 .. Line_Last), Upper2, Num_Last);
+         Ada.Integer_Text_IO.Get (S (1 .. Hyphen1 - 1), X1, Num_Last);
+         Ada.Integer_Text_IO.Get (S (Hyphen1 + 1 .. Comma - 1), X2, Num_Last);
+         Ada.Integer_Text_IO.Get (S (Comma + 1 .. Hyphen2 - 1), Y1, Num_Last);
+         Ada.Integer_Text_IO.Get (S (Hyphen2 + 1 .. Line_Last), Y2, Num_Last);
 
-         if (Lower1 <= Lower2 and then Upper2 <= Upper1) or else
-           (Lower2 <= Lower1 and then Upper1 <= Upper2)
+         if (X1 <= Y1 and then Y2 <= X2) or else
+            (Y1 <= X1 and then X2 <= Y2)
          then
+            -- One segment is a subset of the other
             if Num_Subset_Pairs < Integer'Last then
                Num_Subset_Pairs := Num_Subset_Pairs + 1;
             else
@@ -49,13 +53,12 @@ begin
             end if;
          end if;
 
-         if (Lower1 <= Lower2 and then Upper2 <= Upper1) or else
-           (Lower2 <= Lower1 and then Upper1 <= Upper2) or else
-           (Lower1 <= Lower2 and then Lower2 <= Upper1) or else
-           (Lower2 <= Lower1 and then Lower1 <= Upper2) or else
-           (Lower1 <= Upper2 and then Upper2 <= Upper1) or else
-           (Lower2 <= Upper1 and then Upper1 <= Upper2)
+         if (Y1 <= X1 and then X1 <= Y2) or else
+            (Y1 <= X2 and then X2 <= Y2) or else
+            (X1 <= Y1 and then Y2 <= X2)
          then
+            -- The first segment has at least one endpoint in the second or
+            -- the first segment contains the second, and the two overlap
             if Num_Overlapping_Pairs < Integer'Last then
                Num_Overlapping_Pairs := Num_Overlapping_Pairs + 1;
             else
@@ -70,8 +73,8 @@ begin
    if Error then
       Put_Line ("There was an error.");
    else
-      Put_Line ("Number of subset pairs for part one: " & Num_Subset_Pairs'Image);
-      Put_Line ("Number of overlapping pairs for part two: " & Num_Overlapping_Pairs'Image);
+      Put_Line ("Number of subset pairs for Part 1: " & Num_Subset_Pairs'Image);
+      Put_Line ("Number of overlapping pairs for Part 2: " & Num_Overlapping_Pairs'Image);
    end if;
 
    Close (File);
